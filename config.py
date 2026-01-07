eca1069b658178875c8a8b3de82c5ec65748daf2
lzmouh <lzmouh@gmail.com>
2026-01-07 14:23:20 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+DB_URL = "sqlite:///database/real_estate.db"
+
+UPLOAD_PATH = "assets/uploads/"
+
+EMAIL_SENDER = "LZMOUH@gmail.com"
+EMAIL_PASSWORD = "app_password"
+
+TWILIO_SID = "YOUR_SID"
+TWILIO_TOKEN = "YOUR_TOKEN"
+TWILIO_PHONE = "+123456789"
