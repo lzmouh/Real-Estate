d490997aca31da1d65bcf7d266e58ea5fb3bb847
lzmouh <lzmouh@gmail.com>
2026-01-07 14:49:21 +0300

    Create 3_Property_Detail.py

---
+++ 3_Property_Detail.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from database.db import get_session
+from database.models import Property, Lease
+from services.documents import save_document
+
+flat = st.selectbox("Select Property", [])
+
+db = get_session()
+prop = db.query(Property).filter_by(flat=flat).first()
+lease = db.query(Lease).filter_by(flat=flat).first()
+
+st.header("Property Details")
+st.write(prop.__dict__)
+
+st.header("Lease Details")
+st.write(lease.__dict__)
+
+st.header("Documents")
+file = st.file_uploader("Upload")
+if file:
+    save_document(flat, file)
+    st.success("Uploaded")
